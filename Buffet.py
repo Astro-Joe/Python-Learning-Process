258e9ef4946cee1261fc0f555b28910c6ceb0496
unknown <ilemobayojoseph06@gmail.com>
2022-08-29 19:02:12 +0100

    Using for loop in a buffet situation

---
+++ Buffet.py
@@ -0,0 +1,13 @@
+menu = ('rice', 'beans', 'jollof', 'spaghetti', 'macaroni')
+for food in menu:
+    print(food)
+
+print('\n')
+
+menu = ('rice', 'beans', 'amala', 'spaghetti', 'macaroni')
+for new in menu:
+    print(new)
+if 'spaghetti' in menu:
+    print('\nI love spaghetti!')
+
+
