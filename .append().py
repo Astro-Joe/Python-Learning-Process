30121a6606c18f536b2e4d8ea1ed43ca40ad5820
unknown <ilemobayojoseph06@gmail.com>
2022-08-29 19:05:02 +0100

    Appending inputs to a list

---
+++ .append().py
@@ -0,0 +1,9 @@
+profile = []
+name = input('Enter your name: ')
+class_ = input('Enter your class: ')
+age = input('Enter your age: ')
+profile.append(name)
+profile.append(class_)
+profile.append(age)
+print(profile)
+
