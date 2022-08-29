5ac81f4ed4c66cc835d7886d416779cb3156e6f2
unknown <ilemobayojoseph06@gmail.com>
2022-08-29 21:52:55 +0100

    Program to change normal integers to ordinal numbers.

---
+++ ordinal_numbers.py
@@ -0,0 +1,10 @@
+num = list(range(1,10))
+for nums in num:
+    if nums == 1:
+        print(str(nums)+'st')
+    if nums == 2:
+        print(str(nums) + 'nd')
+    if nums == 3: 
+        print(str(nums) + 'rd')
+    if z
+    
