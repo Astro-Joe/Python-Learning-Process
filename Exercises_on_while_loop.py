7800a673aac8a003c91c9229f888f0ab1c9b256e
unknown <ilemobayojoseph06@gmail.com>
2022-08-29 21:29:25 +0100

    W3schools exercises on loops.

---
+++ Exercises_on_while_loop.py
@@ -0,0 +1,26 @@
+# Write a Python program to find those numbers which are divisible by 7 and multiple of 5, 
+# between 1500 and 2700 (both included)
+
+# for i in range(1500,2701):
+#     if i%7==0 and i%5==0:
+#         print(i)
+
+
+
+#Write a Python program to guess a number between 1 to 9.
+# guess = [1,2,3,4,5,6,7,8,9]
+# active = True
+# while active:
+#     num = int(input('Guess a number: '))
+#     if num not in guess:
+#         continue
+#     else:
+#         print('Well guessed!')
+#         active = False
+this= []
+sth = []
+lis = input()
+for i in lis:
+    sth.append(i)
+# sth.sort(reverse=True)
+print(sth)
