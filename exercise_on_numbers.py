d73576665bf22d5e745f8eafa0b7c9dd90aa6ba7
unknown <ilemobayojoseph06@gmail.com>
2022-08-29 21:27:49 +0100

    Statistical application of python

---
+++ exercise_on_numbers.py
@@ -0,0 +1,19 @@
+#for value in range(1,21):
+  #  print(value)
+
+million = range(1,1000001)
+#for num in million:
+ #   print(num)
+print(min(million))
+print(max(million))
+#print(sum(million))
+print('\n')
+
+odd_numbers = list(range(1,21,2))
+for numbers in odd_numbers:
+    print(numbers)
+print('\n')
+
+threes = list(range(3,31,3))
+for i in threes:
+    print(i)
