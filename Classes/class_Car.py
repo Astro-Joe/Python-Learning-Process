0f6541284b404bda723897769684a521d4a2aaea
unknown <ilemobayojoseph06@gmail.com>
2022-09-30 02:00:59 +0100

    Learning classes

--- Classes/class_Car.py
+++ Classes/class_Car.py
@@ -6,11 +6,18 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
+
 
     def get_descriptive_name(self):
         """Return a neatly formatted descriptive name."""
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
         return long_name.title()
+
+
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
 
 
 my_new_car = Car('audi', 'a4', 2016)
