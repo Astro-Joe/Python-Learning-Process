e2f27c111133b1f51fe68b54f80e2fa600964bda
unknown <ilemobayojoseph06@gmail.com>
2022-10-01 23:49:30 +0100

    modified this file

--- Classes/class_Car.py
+++ Classes/class_Car.py
@@ -2,7 +2,7 @@ class Car():
     """A simple attempt to represent a car."""
 
     def __init__(self, make, model, year):
-        """Initialize attributes to escribe a car."""
+        """Initialize attributes to describe a car."""
         self.make = make
         self.model = model
         self.year = year
@@ -22,3 +22,5 @@ class Car():
 
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+
