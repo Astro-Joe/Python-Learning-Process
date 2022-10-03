56feae7d000033952355a674d20ac1ef8449aa2f
unknown <ilemobayojoseph06@gmail.com>
2022-10-03 22:34:12 +0100

    Overriding methods from the parent class

--- Classes/class_Car.py
+++ Classes/class_Car.py
@@ -7,6 +7,7 @@ class Car():
         self.model = model
         self.year = year
         self.odometer_reading = 0
+        self.gas_tank = 12
 
 
     def get_descriptive_name(self):
@@ -29,13 +30,44 @@ class Car():
         """Add the given amount to the odometer reading"""
         self.odometer_reading += mileage
 
-my_used_car = Car('audi', 'a4', 2016)
-print(my_used_car.get_descriptive_name())
+    
+    def fill_gas_tank(self):
+        print("Your gas tank is ", str(self.gas_tank) + " litres." )
 
-my_used_car.update_odometer(50)
-my_used_car.read_odometer()
 
-my_used_car.increment_odometer(30)
-my_used_car.read_odometer()
 
+
+class ElectricCar(Car):
+    """Represents aspects of a car, specific to electric vehicles"""
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class."""
+        super().__init__(make, model, year)
+        self.battery_size = 70
+
+
+    def describe_battery(self):
+        """Print a statement describing the battery size"""
+        print("This car has a " + str(self.battery_size) + "-KWh battery.")
+
+
+    def fill_gas_tank(self):
+        """Electric cars don't have gas tank."""
+        print("This car deosn't need a gas tank")
+
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
+my_tesla.fill_gas_tank()
+
+
+#my_used_car = Car('audi', 'a4', 2016)
+# print(my_used_car.get_descriptive_name())
+
+# my_used_car.update_odometer(50)
+# my_used_car.read_odometer()
+
+# my_used_car.increment_odometer(30)
+# my_used_car.read_odometer()
+#my_used_car.fill_gas_tank()
 
