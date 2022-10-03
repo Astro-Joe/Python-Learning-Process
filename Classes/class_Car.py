9eac7575c0f805e17f6560f2f4138e809af62d25
unknown <ilemobayojoseph06@gmail.com>
2022-10-03 04:32:23 +0100

    Increamenting an attribute's value through a method.

--- Classes/class_Car.py
+++ Classes/class_Car.py
@@ -23,12 +23,19 @@ class Car():
     def update_odometer(self, mileage):
         """Set the odometer reading to a certain value"""
         self.odometer_reading = mileage
+    
 
+    def increment_odometer(self, mileage):
+        """Add the given amount to the odometer reading"""
+        self.odometer_reading += mileage
 
-my_new_car = Car('audi', 'a4', 2016)
-print(my_new_car.get_descriptive_name())
+my_used_car = Car('audi', 'a4', 2016)
+print(my_used_car.get_descriptive_name())
 
-my_new_car.update_odometer(23)
-# my_new_car.odometer_reading = 23
-my_new_car.read_odometer()
+my_used_car.update_odometer(50)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(30)
+my_used_car.read_odometer()
+
 
