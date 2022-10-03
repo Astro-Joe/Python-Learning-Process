8f63e80d45842f9b61e334a01da438dc28ba987d
unknown <ilemobayojoseph06@gmail.com>
2022-10-03 04:19:12 +0100

    Modifying an attribute's value directly

--- Classes/class_Car.py
+++ Classes/class_Car.py
@@ -14,13 +14,20 @@ class Car():
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
         return long_name.title()
 
-
+    
     def read_odometer(self):
         """Print a statement showing the car's mileage."""
         print("This car has " + str(self.odometer_reading) + " miles on it.")
 
 
+    # def update_odometer(self, mileage):
+    #     """Set the odometer reading to a certain value"""
+    #     self.odometer_reading = mileage
+
+
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
+
+my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
 
