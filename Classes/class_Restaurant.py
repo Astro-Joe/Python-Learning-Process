a76ebc026f13d3180d9c8d402aed9a640b9ef507
unknown <ilemobayojoseph06@gmail.com>
2022-10-03 04:54:02 +0100

    Adding methodtoprint the number of peeps served

--- Classes/class_Restaurant.py
+++ Classes/class_Restaurant.py
@@ -4,6 +4,7 @@ class Restaurant():
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        number_served = 0
 
     def describe_restaurant(self):
         print(self.restaurant_name.title())
@@ -13,13 +14,26 @@ class Restaurant():
         print(self.restaurant_name.title() + ' is open.' + '\n')
 
 
-restaurant = Restaurant('delightsome chicken', 'average')
-restaurant_ = Restaurant('mr biggs', 'fancy')
-restaurant__ = Restaurant("domino's", 'middle class')
-# print(restaurant.restaurant_name)
-# print(restaurant.cuisine_type + "\n")
-restaurant.describe_restaurant()
-restaurant_.describe_restaurant()
-restaurant__.describe_restaurant()
-# restaurant.open_restaurant()
+    def  
+    
+    def set_number_served(self, number):
+        self.number_served = number
+        print(str(self.number_served) + " people have been served in " + self.restaurant_name)
 
+
+    def increament_number_served(self, number):
+        self.number_served += number
+
+
+# restaurant = Restaurant('delightsome chicken', 'average')
+# restaurant_ = Restaurant('mr biggs', 'fancy')
+# restaurant__ = Restaurant("domino's", 'middle class')
+# # print(restaurant.restaurant_name)
+# # print(restaurant.cuisine_type + "\n")
+# restaurant.describe_restaurant()
+# restaurant_.describe_restaurant()
+# restaurant__.describe_restaurant()
+# restaurant.open_restaurant()
+description = Restaurant("Mr biggs", "Fancy")
+# description.number_served = 11
+description.set_number_served(11)
