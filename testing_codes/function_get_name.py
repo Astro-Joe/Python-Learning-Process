7393a0f72ac64765ed58515fd4858b15e22c429d
unknown <ilemobayojoseph06@gmail.com>
2022-10-14 01:15:47 +0100

    Function to get a user's name.

---
+++ testing_codes/function_get_name.py
@@ -0,0 +1,11 @@
+def get_formatted_name(firstName, lastName, middleName=''):
+    """Return a full name neatly formatted"""
+    if middleName:
+        full_name = firstName  + ' ' + middleName + ' ' + lastName
+    else:
+        full_name = firstName + ' ' + lastName
+    return full_name.title()
+
+
+# musician = get_formatted_name('joseph', 'ilemobayo')
+# print('my name is ' + musician)
