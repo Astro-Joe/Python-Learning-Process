ca2533fc17352b59837df82595a9244c21d57e0e
unknown <ilemobayojoseph06@gmail.com>
2023-07-03 05:56:10 +0100

    Added an argument to the function.

--- practice/revision.py
+++ practice/revision.py
@@ -1,8 +1,8 @@
 #How to define a function.
 #You define a function.
 #A function must have a doc string talking about what the functuion is about.
-def greet_user():
+def greet_user(first_name):
     """Display a simple greeting."""
-    print("Hello!")
+    print("Hello " + first_name.title() + "!")
 
-greet_user()
+greet_user("joseph")
