4162168220686b1c5b23b1da9cd7a121e08f7cd7
unknown <ilemobayojoseph06@gmail.com>
2023-07-03 06:12:12 +0100

    added a default value to the function

--- practice/revision.py
+++ practice/revision.py
@@ -1,8 +1,8 @@
 #How to define a function.
 #You define a function.
 #A function must have a doc string talking about what the functuion is about.
-def greet_user(first_name , lastname):
+def greet_user(first_name , lastname='ilemobayo'):
     """Display a simple greeting."""
     print("Hello " + first_name.title() + " " + lastname.title() + "!")
 
-greet_user(lastname='ilemobayo', first_name='joseph')
+greet_user(first_name='joseph')
