7066d093e11d6bbd0636c953d158f2c236f6b2fa
unknown <ilemobayojoseph06@gmail.com>
2023-07-03 06:08:52 +0100

    Added a keyword arguement

--- practice/revision.py
+++ practice/revision.py
@@ -1,8 +1,8 @@
 #How to define a function.
 #You define a function.
 #A function must have a doc string talking about what the functuion is about.
-def greet_user(first_name):
+def greet_user(first_name , lastname):
     """Display a simple greeting."""
-    print("Hello " + first_name.title() + "!")
+    print("Hello " + first_name.title() + " " + lastname.title() + "!")
 
-greet_user("joseph")
+greet_user(lastname='ilemobayo', first_name='joseph')
