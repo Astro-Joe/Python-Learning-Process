9c91e19289b4359bc9aab195cfb5d07a1fe56b70
unknown <ilemobayojoseph06@gmail.com>
2023-07-03 05:48:12 +0100

    Added some comments.

--- practice/revision.py
+++ practice/revision.py
@@ -1,4 +1,6 @@
 #How to define a function.
+#You define a function.
+#A function must have a doc string talking about what the functuion is about.
 def greet_user():
     """Display a simple greeting."""
     print("Hello!")
