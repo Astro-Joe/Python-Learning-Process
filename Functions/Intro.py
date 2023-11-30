b1984b12fdd5bd291380b2f9e789c9650c314bd6
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-11-30 07:53:36 +0100

    Added comments to all th lines of code.

---
+++ Functions/Intro.py
@@ -0,0 +1,7 @@
+def greet_user():  # The keyword "def" is used to inform python that you are defining a function.
+    """Displays a simple greeting""" # A docstring used to describe what the functon does.
+    
+    print("Hello!") # The task the function is designed to carry out.
+    
+greet_user() # Calling the function.
+
