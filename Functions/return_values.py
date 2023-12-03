c9b3ac91379723096e61dc4c34aab0795ff722c4
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-12-03 18:59:28 +0100

    Added a new parameter and made it optional

--- Functions/return_values.py
+++ Functions/return_values.py
@@ -1,14 +1,16 @@
 # Basically,  return values allow the result of your function to be assigned to a variable and 
 #be used in a flexible way in your code.
+# Parameters can be made optional too. It uses the principle of default parameter value.
 
-def get_formatted_name(first_name, last_name):
+def get_formatted_name(first_name, last_name, middle_name= ''): # Here making the default value of middle_name an empty strings makes it an optional parameter
     """ Returns a full name, neatly formatted.
     Contains two parameters to get a full name and return the output of the function to the caller(assigned variable)."""
     
-    full_name = first_name + ' ' + last_name
+    full_name = first_name + ' ' + middle_name + ' ' + last_name
     return full_name.title()
 
 sample_1 = get_formatted_name('joseph', 'ilemobayo')
-print('The name of the guy who wrote this code is ' + sample_1)
-    
-    
+print(sample_1)
+
+sample_2 = get_formatted_name('joseph', 'ilemobayo', 'tolulope')
+print(sample_2)
