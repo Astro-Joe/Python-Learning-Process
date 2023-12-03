0578670b40a4e48e31c9512e737b979235aed18a
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-12-03 17:29:56 +0100

    removed the title method from the animal type output

--- Functions/positional_and _keyword _arguments.py
+++ Functions/positional_and _keyword _arguments.py
@@ -2,6 +2,7 @@
 def name_of_pet(animal_type, pet_name):
     """Prints out the name and type of pet."""
     
-    print('The name of my ' + animal_type.title() + ' is ' + pet_name)
+    print('The name of my ' + animal_type + ' is ' + pet_name)
     
-name_of_pet()
+name_of_pet('dog', 'trump')# using positional argument format
+name_of_pet(pet_name= 'trump', animal_type='dog') # Using keyword argument format
