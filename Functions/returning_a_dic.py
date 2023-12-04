ead33629d4424cbe64970b499abb6f3abe853e58
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-12-04 01:28:25 +0100

    Modified the function.

--- Functions/returning_a_dic.py
+++ Functions/returning_a_dic.py
@@ -1,15 +1,13 @@
 # A function can be made to return complex data structures like lists and dics
 
 def build_car(name_of_car='', model_of_car=''):
-    """Returns a list of info about inputed cars"""
+    """Returns a list of info about inputed car"""
     
     info = []
     car = {'name':name_of_car, 'model' : model_of_car}
-    if name_of_car and model_of_car:
-        info.append(car)
-    else:
-        info[:].rm()
+    info.append(car)
     
+    print(info)
     return info
 
 sample_0 = build_car('Highlander', '2023')
