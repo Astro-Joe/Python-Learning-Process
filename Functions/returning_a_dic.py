4a7a15ae939838e1e7d9c95a090ff0a224a3362b
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-12-04 02:14:40 +0100

    Created a function that returns a dic and manipulated the function using a while loop.

--- Functions/returning_a_dic.py
+++ Functions/returning_a_dic.py
@@ -1,14 +1,31 @@
-# A function can be made to return complex data structures like lists and dics
+# A function can be made to return complex data structures like lists and dictionaries
+# Below is how a simple function that returns a dic. can be manipulated to get a desired result.
 
 def build_car(name_of_car='', model_of_car=''):
-    """Returns a list of info about inputed car"""
-    
-    info = []
+    """Returns a dictionary containing the name and model of a car."""
     car = {'name':name_of_car, 'model' : model_of_car}
-    info.append(car)
-    
-    print(info)
-    return info
+    return car
 
-sample_0 = build_car('Highlander', '2023')
-sample_1 = build_car('Rover', '2013')
+
+
+info = [] 
+
+while True:
+    print('\nEnter "q" to quit the process')
+    car_name = input('Enter the name of the car: ')
+    if car_name == 'q':
+        break
+    
+    print('Enter "q" to quit')
+    car_model = input('Enter the model of the car: ')
+    if car_model == 'q':
+        break
+    
+    
+    sample_0 = build_car(car_name, car_model)
+    info.append(sample_0)
+print(info)
+    
+    
+
+    
