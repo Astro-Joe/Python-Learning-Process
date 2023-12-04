eff5514b4fb908e8600c4567abfd9e29c2f318b1
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-12-04 01:29:21 +0100

    Tesed an idea

--- practice/revision.py
+++ practice/revision.py
@@ -1,8 +1,10 @@
 #How to define a function.
 #You define a function.
 #A function must have a doc string talking about what the functuion is about.
-def greet_user(first_name , lastname='ilemobayo'):
-    """Display a simple greeting."""
-    print("Hello " + first_name.title() + " " + lastname.title() + "!")
+# def greet_user(first_name , lastname='ilemobayo'):
+#     """Display a simple greeting."""
+#     print("Hello " + first_name.title() + " " + lastname.title() + "!")
 
-greet_user(first_name='joseph')
+# greet_user(first_name='joseph')
+name = [{'first':'Joseph' }]
+print(name)
