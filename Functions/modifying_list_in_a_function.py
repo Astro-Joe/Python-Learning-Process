b5730f8290c9a3a980384e4692189f3f7d9588e7
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-12-04 23:29:32 +0100

    Restructured the comments

--- Functions/modifying_list_in_a_function.py
+++ Functions/modifying_list_in_a_function.py
@@ -1,7 +1,4 @@
-# A function that shows shows printing 3D designs.
-
-# This will contain two functions.
-# The first will show that the 3D designs are being printed.
+# A function that shows the process of printing 3D designs.
 
 def design_printer(uncompleted_design, completed_design):
     """Shows the process of printing a 3D design."""
