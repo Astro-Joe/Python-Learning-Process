8a1834b29b6e5642ab108a52b48fc3263b656db3
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-12-05 23:28:25 +0100

    Used a function in another script as a module.

---
+++ Functions/importing_modules.py
@@ -0,0 +1,8 @@
+# Modules are functions stored in another file and are imported to keep the main script organized and less messy.
+
+import returning_a_list as rt_list # rt_list ia an alias used to represent the module.e
+
+
+usernames = ['joseph', 'tolulope', 'damilare', 'ifeoluwa']
+rt_list.greet_users(usernames)
+
