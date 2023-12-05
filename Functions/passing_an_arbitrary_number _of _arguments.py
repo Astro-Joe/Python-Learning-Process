56b5b057d93531710bf5ab2b44b6622304569903
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-12-05 16:35:22 +0100

    Added a positional parameter.

--- Functions/passing_an_arbitrary_number _of _arguments.py
+++ Functions/passing_an_arbitrary_number _of _arguments.py
@@ -1,13 +1,14 @@
 # In this case the function is allowed to accept as much arguments as possible that is needed for the program to work.
 
 # Like in making a pizza, you can't know before hand how many topping the customer wants
+# Positional and arbitrary arguments can also be used together 
 
-def make_pizza(*toppings): # The parameter 'toppings' is represented as a tuple
+def make_pizza(size, *toppings): # The parameter 'toppings' is represented as a tuple
     """Print the toppings that have been requested"""
     
-    print("\nMaking pizza with the following toppings:")
+    print("\nMaking a " + str(size) +  "-inch pizza with the following toppings:")
     for topping in toppings:
         print('\t-' + topping.title())
     
-make_pizza('pepperoni')
-make_pizza('munshrooms', 'green peppers', 'extra cheese')
+make_pizza(18, 'pepperoni')
+make_pizza(20, 'munshrooms', 'green peppers', 'extra cheese')
