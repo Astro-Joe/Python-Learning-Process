0d486963fd13fa823c1108b4f395d33d4110adb4
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-12-06 13:14:34 +0100

    Initialized the attributes of the class

--- Classes/class_Dog.py
+++ Classes/class_Dog.py
@@ -1,4 +1,8 @@
 class Dog():
     """A simple attempt to model a dog."""
     
-    
+    def __init__(self, name, age):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+        
