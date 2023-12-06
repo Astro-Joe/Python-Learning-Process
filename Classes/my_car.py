7921c6b50e49bf20145103e9e9bfd9319a9e3a9b
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-12-06 19:32:09 +0100

    Tested a method.

--- Classes/my_car.py
+++ Classes/my_car.py
@@ -4,5 +4,8 @@ from class_Car import Car  # imports the class 'Car' from 'class_Car.py' module
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
 
-my_new_car.odometer_reading = 23
+# my_new_car.odometer_reading = -29
+# print(my_new_car.read_odometer())
+
+my_new_car.update_odometer(30)
 print(my_new_car.read_odometer())
