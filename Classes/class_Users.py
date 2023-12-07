97e6b32541521dad491600aa90bd7e413376a283
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-12-07 21:12:43 +0100

    Created instances to test method formerly created.

--- Classes/class_Users.py
+++ Classes/class_Users.py
@@ -1,10 +1,10 @@
 class User():
-    def __init__(self, first_name, last_name, location, usr_name, login_attempts):
+    def __init__(self, first_name, last_name, location, usr_name):
         self.first_name = first_name.title()
         self.last_name = last_name.title()
         self.location = location.title()
         self.usr_name = usr_name.title()
-        self.login_attempts = login_attempts
+        self.login_attempts = [0]
 
 
     def describe_user(self):
@@ -17,17 +17,18 @@ class User():
         full_name = self.first_name + ' ' + self.last_name
         print("\nHello", full_name + '!.')
 
-    # def increment_login_attempts(self):
-    #     attempt = [0,1]
-    #     item = 1
-    #     if item > 0:
-    #         item+=1
-    #         attempt[0] = item
-    #         self.login_attempts = attempt[0]
+    def increment_login_attempts(self):
+        flag = True 
+        while flag:
+            increment = self.login_attempts + 1
+            self.login_attempts = increment
+            flag = False
+        print('There have been ' + str(increment) + ' login attempts')
+        
  
 
-    def reset_login_attempts(self):
-        self.login_attempts = 0
+    def reset_login_attempts(self, attempt):
+        self.login_attempts[0] = 0
 
 class Privileges():
     def __init__(self, *privileges):
@@ -53,13 +54,13 @@ class Admin(Privileges):
 
 
 
+user = User('joseph', 'ilemobayo', 'lagos', 'astrojoe')
 
-
-user = Admin('can add post', 'can ban user', 'can delete post')
+# user = Admin('can add post', 'can ban user', 'can delete post')
 # user.describe_user()
 # user.greet_user()
-# user.increment_login_attempts()
-# print(user.login_attempts)
+user.increment_login_attempts()
+print(user.login_attempts)
 # user.reset_login_attempts()
 # print(user.login_attempts)
-user.privileges.show_privileges()
+# user.privileges.show_privileges()
