cfdbb41d91a23f0983ab6684babf8b8315411bcb
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-12-08 16:07:46 +0100

    Deleted Admin and Priviledges class.

--- Classes/class_Users.py
+++ Classes/class_Users.py
@@ -25,42 +25,18 @@ class User():
             flag = False
         print('There have been ' + str(increment) + ' login attempts')
         
- 
-
     def reset_login_attempts(self, attempt):
         self.login_attempts[0] = 0
 
-class Privileges():
-    def __init__(self, *privileges):
-        self.privileges = list(privileges)
+
+
     
-
-
-    def show_privileges(self):
-            print("These are the list of Admin privileges: ")
-            for privilege in self.privileges:
-                print("\t-" + str(privilege.title()))
-
-
-class Admin(Privileges):
-    def __init__(self, *user):
-        self.user = user
-        super().__init__('privileges')
-        self.user = list(self.privileges)
-        self.privileges = Privileges()
+    
     
 
     
 
 
 
-user = User('joseph', 'ilemobayo', 'lagos', 'astrojoe')
 
-# user = Admin('can add post', 'can ban user', 'can delete post')
-# user.describe_user()
-# user.greet_user()
-user.increment_login_attempts()
-print(user.login_attempts)
-# user.reset_login_attempts()
-# print(user.login_attempts)
-# user.privileges.show_privileges()
+
