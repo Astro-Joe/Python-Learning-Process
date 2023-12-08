e9b691fb764573dfa8bdded9dba1962d092505e2
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-12-08 17:24:44 +0100

    Added docstrings for all methods in User.

--- Classes/class_Users.py
+++ Classes/class_Users.py
@@ -1,33 +1,39 @@
 class User():
-    def __init__(self, first_name, last_name, location, usr_name):
-        self.first_name = first_name.title()
-        self.last_name = last_name.title()
-        self.location = location.title()
-        self.usr_name = usr_name.title()
-        self.login_attempts = [0]
+        """Modelling a User"""
+        def __init__(self, first_name, last_name, location, usr_name):
+            """Initializing the attributes of User"""
+            self.first_name = first_name.title()
+            self.last_name = last_name.title()
+            self.location = location.title()
+            self.usr_name = usr_name.title()
+            self.login_attempts = [0]
 
 
-    def describe_user(self):
-        full_name = self.first_name + ' ' + self.last_name
-        print("Full name:", full_name)
-        print("Username:", self.usr_name)
-        print("Location:", self.location)
+        def describe_user(self):
+            """Describes the user"""
+            full_name = self.first_name + ' ' + self.last_name
+            print("Full name:", full_name)
+            print("Username:", self.usr_name)
+            print("Location:", self.location)
 
-    def greet_user(self):
-        full_name = self.first_name + ' ' + self.last_name
-        print("\nHello", full_name + '!.')
+        def greet_user(self):
+            """Show a greeting for a user"""
+            full_name = self.first_name + ' ' + self.last_name
+            print("\nHello", full_name + '!.')
 
-    def increment_login_attempts(self):
-        flag = True 
-        while flag:
-            increment = self.login_attempts + 1
-            self.login_attempts = increment
-            flag = False
-        print('There have been ' + str(increment) + ' login attempts')
-        
-    def reset_login_attempts(self, attempt):
-        self.login_attempts[0] = 0
-        pass
+        def increment_login_attempts(self):
+            """Shows the number of login attempts"""
+            flag = True 
+            while flag:
+                increment = self.login_attempts + 1
+                self.login_attempts = increment
+                flag = False
+            print('There have been ' + str(increment) + ' login attempts')
+            
+        def reset_login_attempts(self, attempt):
+            """Resets the number of login attempts"""
+            self.login_attempts[0] = 0
+            pass
 
 
 class Admin(User):
