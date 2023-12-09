9260a7acde0feb0221fa1d40d0de9b92278869d0
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-12-09 12:04:39 +0100

    Tested the methods in the Battery class.

--- Classes/my_car.py
+++ Classes/my_car.py
@@ -1,11 +1,17 @@
-from class_Car import Car  # imports the class 'Car' from 'class_Car.py' module
+from class_Car import *  # imports the class 'Car' from 'class_Car.py' module
 
 
-my_new_car = Car('audi', 'a4', 2016)
-print(my_new_car.get_descriptive_name())
+# my_new_car = Car('audi', 'a4', 2016)
+# print(my_new_car.get_descriptive_name())
 
 # my_new_car.odometer_reading = -29
 # print(my_new_car.read_odometer())
 
-my_new_car.update_odometer(30)
-print(my_new_car.read_odometer())
+# my_new_car.update_odometer(30)
+# print(my_new_car.read_odometer())
+tesla = ElectricCar('tesla', 'f9', '2019')
+tesla.battery.get_range()
+tesla.battery.describe_battery()
+tesla.battery.upgrade_battery()
+tesla.battery.get_range()
+tesla.battery.describe_battery()
